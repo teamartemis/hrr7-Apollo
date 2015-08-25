5e0574850b5cbfa88de63015c53ca54f7f4594de
David Craven <david@craven.ch>
2015-08-24 17:44:51 -0700

    (refactor) Remove leaderboard

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var morgan = require('morgan');                   // log requests to the console (express4)
-var bodyParser = require('body-parser');          // pull information from HTML POST (express4)
-var methodOverride = require('method-override');  // simulate DELETE and PUT (express4)
-var api = require('./server/controllers/routes');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 mongoose.connect(process.env.DB_URI || 'mongodb://localhost/apollo');
 var db = mongoose.connection;
@@ -13,13 +12,12 @@ db.once('open', function() {
 });
 
 var app = express();
-app.use(express.static('client'));                              // set the static files location (e.g. /client/game will be /game)
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(express.static('client'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
-app.use('/api', api);
 
 var server = require('http').Server(app);
 var io = require('./server/socket')(server);
